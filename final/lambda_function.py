1b6dd83c4cf6be92a034c4ae2d35d8287b7386cd
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2019-09-05 11:18:42 -0400

    [ ADD ] module-1 and module-2 are fully tested and good to go. still working on module-3

--- final/lambda_function.py
+++ final/lambda_function.py
@@ -44,6 +44,7 @@ class HasBirthdayLaunchRequestHandler(AbstractRequestHandler):
     Handler for launch after they have set their birthday
     """
     def can_handle(self, handler_input):
+        # extract persistent attributes and check if they are all present
         attr = handler_input.attributes_manager.persistent_attributes
         attributes_are_present = ("year" in attr and "month" in attr and "day" in attr)
 
@@ -124,15 +125,18 @@ class BirthdayIntentHandler(AbstractRequestHandler):
     def handle(self, handler_input):
         slots = handler_input.request_envelope.request.intent.slots
 
+        # extract slot values
         year = slots["year"].value
         month = slots["month"].value
         day = slots["day"].value
 
+        # save slots into session attributes
         session_attr = handler_input.attributes_manager.session_attributes
         session_attr['year'] = year
         session_attr['month'] = month
         session_attr['day'] = day
 
+        # save session attributes as persistent attributes
         handler_input.attributes_manager.persistent_attributes = session_attr
         handler_input.attributes_manager.save_persistent_attributes()
 
@@ -159,26 +163,26 @@ class HelpIntentHandler(AbstractRequestHandler):
 
 
 class CatchAllExceptionHandler(AbstractExceptionHandler):
-    """Catch all exception handler, log exception and
+    """
+    Catch all exception handler, log exception and
     respond with custom message.
     """
     def can_handle(self, handler_input, exception):
         return True
 
     def handle(self, handler_input, exception):
-        print("In CatchAllExceptionHandler")
-        print(exception)
+
         speak_output = "Sorry, I couldn't understand what you said. Please try again."
         handler_input.response_builder.speak(speak_output).ask(speak_output)
         return handler_input.response_builder.response
 
-
+# register request / intent handlers
 sb.add_request_handler(HasBirthdayLaunchRequestHandler())
 sb.add_request_handler(LaunchRequestIntentHandler())
 sb.add_request_handler(BirthdayIntentHandler())
 sb.add_request_handler(HelpIntentHandler())
 
-# Register exception handlers
+# register exception handlers
 sb.add_exception_handler(CatchAllExceptionHandler())
 
 lambda_handler = sb.lambda_handler()
